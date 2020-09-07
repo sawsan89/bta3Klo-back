0741a64e9362637d14eaf2ad2343e000b637d798
Mayis-Mazin <you@example.com>
2020-09-07 19:18:36 +0300

    Data Base

---
+++ keys.js
@@ -0,0 +1,5 @@
+module.exports={
+    MONGOURI:"mongodb+srv://Mayis:wrKo6iNjAGYgnAck@cluster0.fpidh.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
+
+//wrKo6iNjAGYgnAck
--- server.js
+++ server.js
@@ -2,7 +2,27 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
 var app = express();
-var port =  8080;
+const mongoose =require('mongoose');
+const { MONGOURL } = require('./keys');
+
+require('./models/user')
+//mongoose.model('User')
+
+const PORT = process.env.PORT || 8080
+const {MONGOURI} = require('./keys')
+
+
+mongoose.connect(MONGOURI,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true
+
+})
+mongoose.connection.on('connected',()=>{
+    console.log("conneted to mongo yeahh")
+})
+mongoose.connection.on('error',(err)=>{
+    console.log("err connecting",err)
+})
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -13,6 +33,6 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
   });
 
-app.listen(port, () => {
-    console.log(`Server is running on ${port} Visit https://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT} Visit https://localhost:${PORT}`)
 })
