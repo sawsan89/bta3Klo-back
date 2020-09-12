8ffe3aa0ba29fcd8cd4b73ab8e81aec6bbcae037
unknown <sawsan.h.m.89@gmail.com>
2020-09-12 12:49:49 +0300

    db added

---
+++ database/index.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const url =
+    "mongodb+srv://smunawer:smunawer@bta3kolo.vymhw.gcp.mongodb.net/bta3kolo?retryWrites=true&w=majority";
+const { MongoClient } = require("mongodb");
+const dotenv = require('dotenv');
+
+dotenv.config();
+const uri = process.env.MONGO;
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+});
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+    console.log("We are connected");
+});
+module.exports = db;
