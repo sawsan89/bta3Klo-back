81d2e29445e16ff557ce855a6ad54c044b629eae
unknown <sawsan.h.m.89@gmail.com>
2020-09-15 16:43:13 +0300

    db connected

--- server.js
+++ server.js
@@ -2,31 +2,31 @@ const express = require('express');
 const app = express();
 const mongoose =require('mongoose');
 const PORT = process.env.PORT || 8080
-const { MONGOURI } = require('./keys');
-
+const mongo = require("./database/index");
+const db = require("./database/models");
 
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 
 
-require('./models/user')
+//require('./models/user')
 app.use(express.json())
 app.use(require('./routes/auth'))
 
 
 
-mongoose.connect(MONGOURI,{
-    useNewUrlParser:true,
-    useUnifiedTopology: true
+// mongoose.connect(MONGOURI,{
+//     useNewUrlParser:true,
+//     useUnifiedTopology: true
 
-})
-mongoose.connection.on('connected',()=>{
-    console.log("conneted to mongo yeahh")
-})
-mongoose.connection.on('error',(err)=>{
-    console.log("err connecting",err)
-})
+// })
+// mongoose.connection.on('connected',()=>{
+//     console.log("conneted to mongo yeahh")
+// })
+// mongoose.connection.on('error',(err)=>{
+//     console.log("err connecting",err)
+// })
 
 app.use(cors())
 app.use(bodyParser.json())
@@ -40,3 +40,6 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on ${PORT} Visit https://localhost:${PORT}`)
 })
+
+//old connection
+//const { MONGOURI } = require('./keys');
